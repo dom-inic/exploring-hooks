3e5ba026bdf802c9ce6b85e441406c2d4939083a
dominic <47415866+dom-inic@users.noreply.github.com>
2021-08-12 08:38:51 +0300

    preventing memory leak when using useEffect

--- src/datafetchinghooksuseeffect.js
+++ src/datafetchinghooksuseeffect.js
@@ -4,6 +4,11 @@ export default function datafetchinghooksuseeffect() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
+//         react hook take in a function, we can make it asyncronous
+        const fuctionFetch = async () => {
+            const response = await axios.get(api_url);
+            setData(response.data);
+        }
         fetch("http://localhost:8000//api/users/")
         .then(response => response.json())
         .then(data => setData(data));
@@ -11,8 +16,9 @@ export default function datafetchinghooksuseeffect() {
 
         return () => socket.disconnect();
         
-
-    });
+// we have to turn an empty array so as the hook to run once
+//  this would prevent memory leak in our application
+    }, []);
     return (
         <div>
             <ul>
